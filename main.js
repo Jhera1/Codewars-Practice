1718fc8e3b03c478e71df5c34777746813a5394e
Justin Armstrong <armstrongjustin034@gmail.com>
2024-12-22 20:08:25 -0800

    canYouPlant 390

--- main.js
+++ main.js
@@ -364,10 +364,27 @@
 // console.log(gcdOfStrings("ababab", "ab")); 
 // console.log(gcdOfStrings("LEET", "")); 
 
-var kidsWithCandies = function(candies, extraCandies) {
-  // loop through candies array
-  // check if candies[i] + extraCandies is the largest in array
-  // if so push true to new array 
-  // if not push false to new array 
+// var kidsWithCandies = function(candies, extraCandies) {
+//   // loop through candies array
+//   // check if candies[i] + extraCandies is the largest in array
+//   // if so push true to new array 
+//   // if not push false to new array 
+//     return candies.map(c => (c + extraCandies) >= Math.max(...candies));
+//   };
+//   console.log(kidsWithCandies([10, 3, 6, 3], 5));
 
-};
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// You have a long flowerbed in which some of the plots are 
+// planted, and some are not. However, flowers cannot be 
+// planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, 
+// where 0 means empty and 1 means not empty, 
+// and an integer n, return true if n new flowers can be planted 
+// in the flowerbed without violating the no-adjacent-flowers 
+// rule and false otherwise.
+
+const canYouPlant = (flowerBed, n) => 
